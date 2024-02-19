7b1abf5ee80a0aab0deca88e4fafff788d5da552
Hector Matus <{ID}+{username}@users.noreply.github.com>
2024-02-19 16:12:20 -0600

    Added error handling view

--- src/components/layouts/error/ErrorLayout.tsx
+++ src/components/layouts/error/ErrorLayout.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
-
-const ErrorLayout = () => {
+type ErrorLayoutP = {
+  error: Error & { digest?: string }
+  reset: () => void
+}
+const ErrorLayout = ({error}: ErrorLayoutP) => {
   return (
     <main>
-      <div className="flex items-center justify-center flex-1">
-        <Image sizes="full" alt="Not Found" src={"/images/notFound.png"} />
+      <div className="flex flex-col items-center justify-center flex-1">
+        <Image width={200} height={200} alt="Not Found" src={"/images/notFound.png"} />
+        <h1>{`${error}`}</h1>
       </div>
     </main>
   );
